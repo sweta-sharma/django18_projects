682affe6ca4141ba13803d6fa7ac58cb605a7749
Sweta Sharma <sharmasweta289@gmail.com>
2017-12-04 11:37:21 +0530

    initial

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/this/Documents/repos/django_apps/django18_project/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
